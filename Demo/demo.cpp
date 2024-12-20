a5745f3f167a712f4f97f9ca3b78de97c5d793e8
Martin Gäckler <git@gaeckler.at>
2024-12-20 09:40:23 +0100

    Update demo.cpp

--- Demo/demo.cpp
+++ Demo/demo.cpp
@@ -5,12 +5,22 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 {
   MSG  msg ;    
   WNDCLASS wc = {0};
+  wc.style			= CS_GLOBALCLASS;
   wc.lpszClassName = TEXT( "Buttons" );
   wc.hInstance     = hInstance ;
   wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
   wc.lpfnWndProc   = WndProc ;
   wc.hCursor       = LoadCursor(0, IDC_ARROW);
   RegisterClass(&wc);
+
+  wc.style			= CS_GLOBALCLASS;
+  wc.lpszClassName = TEXT( "Frame" );
+  wc.hInstance     = hInstance ;
+  wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
+  wc.lpfnWndProc   = WndProc ;
+  wc.hCursor       = LoadCursor(0, IDC_ARROW);
+  RegisterClass(&wc);
+
   HWND hwnd = CreateWindow( wc.lpszClassName, TEXT("Buttons"),
                 WS_OVERLAPPEDWINDOW | WS_VISIBLE,
                 150, 150, 230, 150, 0, 0, hInstance, 0);  
@@ -23,20 +33,25 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
   }
   return (int) msg.wParam;
 }
+
 LRESULT CALLBACK WndProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam )
 {
   switch(msg)  
   {
 	case WM_CREATE:
 	{
+        HWND frame = CreateWindowEx(0, TEXT("Frame"), TEXT("&Beep"),    
+		             WS_VISIBLE | WS_CHILD,
+		             0, 0, 230, 130,        
+		             hwnd, 0, NULL, NULL);    
         CreateWindow(TEXT("button"), TEXT("&Beep"),    
-		             WS_VISIBLE | WS_CHILD ,
+		             WS_VISIBLE | WS_CHILD | WS_TABSTOP,
 		             20, 50, 80, 25,        
-		             hwnd, (HMENU) 1, NULL, NULL);    
+		             frame, (HMENU) 1, NULL, NULL);    
 	    CreateWindow(TEXT("button"), TEXT("&Quit"),    
-		             WS_VISIBLE | WS_CHILD ,
+		             WS_VISIBLE | WS_CHILD | WS_TABSTOP ,
 		             120, 50, 80, 25,        
-		             hwnd, (HMENU) 2, NULL, NULL);    
+		             frame, (HMENU) 2, NULL, NULL);    
 	    break;
 	}
     case WM_COMMAND:
